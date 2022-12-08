38eccbc1cbf9574508f0a843836d981bf0fec503
Rizlan Tamima <rizlan@ojekbro.com>
2022-12-08 16:05:54 +0700

    Initial Commit

---
+++ sdl-tutorial/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sdl-tutorial
+//
+//  Created by Rizlan Tamima on 08/12/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
